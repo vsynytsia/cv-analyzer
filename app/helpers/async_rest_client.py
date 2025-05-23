71a032522985117ff4adabb331fdc864ad39a7f3
Vladyslav Synytsia <vladyslav_synytsia@epam.com>
2025-05-23 16:19:07 +0300

    chore: abstract away http client

---
+++ app/helpers/async_rest_client.py
@@ -0,0 +1,17 @@
+import abc
+import logging
+
+import httpx
+from fake_useragent import UserAgent
+
+__all__ = ["AsyncRESTClient"]
+
+
+class AsyncRESTClient(abc.ABC):  # noqa: B024
+    def __init__(self, timeout_seconds: int = 60) -> None:
+        self._timeout = httpx.Timeout(timeout_seconds)
+        self._headers = {"User-Agent": UserAgent().random}
+
+        self._session = httpx.AsyncClient()
+
+        self._logger = logging.getLogger(self.__class__.__name__)
--- app/helpers/vacancy/vacancy_scraping.py
+++ app/helpers/vacancy/vacancy_scraping.py
@@ -1,27 +1,21 @@
 import logging
 from collections.abc import Iterable
 
-import httpx
-from fake_useragent import UserAgent
-
 from app.helpers import utils
-from app.helpers.scraping import JobSiteScraperFactory
-from app.models.domain import JobSearchFilter, Vacancy
+from app.helpers.async_rest_client import AsyncRESTClient
+from app.helpers.html_parsing import JobSiteHtmlParserFactory
+from app.models.domain import Vacancy, VacancySearchFilter
 
 __all__ = ["VacancyScraper"]
 
 
-HTTP_TIMEOUT = 30
-
-
-class VacancyScraper:
+class VacancyScraper(AsyncRESTClient):
     def __init__(self) -> None:
-        self._session = httpx.AsyncClient(
-            timeout=httpx.Timeout(HTTP_TIMEOUT), headers={"User-Agent": UserAgent().random}
-        )
+        super().__init__()
+
         self._logger = logging.getLogger(self.__class__.__name__)
 
-    async def fetch_vacancies(self, filters: Iterable[JobSearchFilter]) -> list[Vacancy]:
+    async def fetch_vacancies(self, filters: Iterable[VacancySearchFilter]) -> list[Vacancy]:
         filtered_vacancies_urls = await utils.gather(
             items=filters,
             coro_factory=lambda filter_: self._fetch_vacancies_urls(filter_),
@@ -29,19 +23,19 @@ class VacancyScraper:
         )
         filtered_vacancies = await utils.gather(
             items=filtered_vacancies_urls,
-            coro_factory=lambda url: self._fetch_vacancy_by_url(url),
+            coro_factory=lambda url: self._fetch_vacancy(url),
             post_processor=lambda x: utils.filter_exceptions(x),
         )
 
         self._logger.info("Fetched %d vacancies from %d URLs", len(filtered_vacancies), len(filtered_vacancies_urls))
         return filtered_vacancies
 
-    async def _fetch_vacancies_urls(self, filter_: JobSearchFilter, limit: int | None = None) -> list[str]:
-        scraper = JobSiteScraperFactory.from_search_filter(filter_)
-        response = await self._session.get(scraper.site_jobs_url, params=scraper.build_http_request_params(filter_))
-        return scraper.extract_vacancy_urls(response.content, limit)
+    async def _fetch_vacancies_urls(self, filter_: VacancySearchFilter) -> list[str]:
+        parser = JobSiteHtmlParserFactory.from_search_filter(filter_)
+        response = await self._session.get(parser.site_vacancies_url, params=filter_.to_http_request_params())
+        return parser.parse_vacancy_urls(response.content)
 
-    async def _fetch_vacancy_by_url(self, url: str) -> Vacancy:
+    async def _fetch_vacancy(self, url: str) -> Vacancy:
         response = await self._session.get(url)
-        scraper = JobSiteScraperFactory.from_vacancy_url(url)
-        return scraper.parse_vacancy_details(response.content, url)
+        parser = JobSiteHtmlParserFactory.from_vacancy_url(url)
+        return parser.parse_vacancy_details(url, response.content)
